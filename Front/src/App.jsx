39dc08c87203ac3003dcbfbddec78de4606f8290
LouisGodfrin <l.godfrin@yahoo.fr>
2024-06-10 17:54:20 +0200

    Changes of Front to implement the form and the map

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,33 +1,52 @@
+import Fuse from 'fuse.js'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {MapContainer,TileLayer,Marker,Popup} from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 import './App.css'
 
+// const data = "Parys"
+
+// const fuse = new Fuse(data, {
+//   // The algorithms to use for fuzzy matching.
+//   algorithms: ["levenshtein", "jaro-winkler"],
+//   // The minimum similarity score required for a match.
+//   minScore: 70,
+// });
+
+// // Search for the string "bar".
+// const results = fuse.search("bar");
+
+const MapComponent = () => {
+  return (
+    <MapContainer center={[51.505, -0.09]} zoom={9} style={{ height: "600px", width: "99%" }}>
+      <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Indiquez votre destination</h1>
+        <div className="App">
+          <form>
+              <label>Lieu de départ: <input type = "texte" ></input></label>
+              <label>Lieu d'arrivé: <input type = "texte" ></input></label>
+              <input type = "submit" value = "Rechercher le plus court chemin"></input> 
+          </form>
+        </div>
+        <MapComponent/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
