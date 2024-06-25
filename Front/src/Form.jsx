f886d3cf1dcc83edf9d99dc0c1cc580bddc54a0e
aldesse <Bussiere.alexandre@gmail.com>
2024-06-25 19:18:47 +0200

    Preparation pour Dikstra + bug fix sur l'autocompletion

--- Front/src/Form.jsx
+++ Front/src/Form.jsx
@@ -1,3 +1,4 @@
+// Form.js
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import AutoComplet from './AutoComplet';
@@ -8,33 +9,46 @@ import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
 const Formulaire = () => {
-
     const [formData, setFormData] = useState({
         stopName: '',
         lat: '',
         lon: '',
+        lieuDepart: '',
+        lieuArrivee: '',
     });
-
-    useState(() => {
-        const storedFormData = localStorage.getItem('formData');
-        if (storedFormData) {
-            setFormData(JSON.parse(storedFormData));
-            console.log("Tout dans le storedFormData qui prend les info de formData")
-            console.log(storedFormData)
-        }
-        else {
-            console.log("Rien dans le storedFormData qui prend les info de formData")
-        }
-    }, []);
 
     const [options, setOptions] = useState(false);
     const [departureTime, setDepartureTime] = useState(null);
     const [arrivalTime, setArrivalTime] = useState(null);
     const [selectedDate, setSelectedDate] = useState(null);
 
+    const Dikstra = () => {
+        let Heure = '';
+        const Date = selectedDate;
+        if (departureTime !== null) {
+            Heure = departureTime;
+        } else {
+            Heure = arrivalTime;
+        }
+
+        const StationDepart = formData.lieuDepart;
+        const StationArriver = formData.lieuArrivee;
+        console.log(`Dikstra = \n${Date}\n${Heure}\n${StationDepart}\n${StationArriver}`);
+    };
+
+    useEffect(() => {
+        const storedFormData = localStorage.getItem('formData');
+        if (storedFormData) {
+            setFormData(JSON.parse(storedFormData));
+            console.log("Tout dans le storedFormData qui prend les info de formData");
+            console.log(storedFormData);
+        } else {
+            console.log("Rien dans le storedFormData qui prend les info de formData");
+        }
+    }, []);
+
     const Options = () => {
-        if(!options)
-        {
+        if (!options) {
             clearSelection();
         }
         setOptions(!options);
@@ -43,6 +57,7 @@ const Formulaire = () => {
     const handleDateChange = (date) => {
         setSelectedDate(date);
     };
+
     const handleDepartureTimeChange = (time) => {
         setDepartureTime(time);
         setArrivalTime(null); // Assure qu'une seule option est sélectionnée à la fois
@@ -59,43 +74,57 @@ const Formulaire = () => {
         setArrivalTime(null);
     };
 
-    // const handleSubmit = (e) => {
-    //     e.preventDefault();
-    //     localStorage.setItem('formData', JSON.stringify(formData)); ? 
-    //      Pour envoyer à Djisktra les infos du form
-    // };
+    const handleLieuDepartChange = (value) => {
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            lieuDepart: value,
+        }));
+    };
+
+    const handleLieuArriveeChange = (value) => {
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            lieuArrivee: value,
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        localStorage.setItem('formData', JSON.stringify(formData));
+        Dikstra(); // Appel de Dikstra après avoir enregistré les données dans localStorage
+    };
 
     return (
-        <div id = "Form">
-            <form id = "Main_Form">
-                <h1 className = "TitleForm">Votre parcours</h1>
-                <label className = "lieu_de_depart">
+        <div id="Form">
+            <form id="Main_Form" onSubmit={handleSubmit}>
+                <h1 className="TitleForm">Votre parcours</h1>
+                <label className="lieu_de_depart">
                     Lieu de départ :
                 </label>
-                <AutoComplet id = "AutoComplet 1" />
+                <AutoComplet id="AutoComplet1" onChange={handleLieuDepartChange} />
                 <br />
-                <label className = "lieu_Arrivee">
-                    Lieu d'arrivé :
+                <label className="lieu_Arrivee">
+                    Lieu d'arrivée :
                 </label>
-                <AutoComplet id = "AutoComplet 2" />
+                <AutoComplet id="AutoComplet2" onChange={handleLieuArriveeChange} />
                 <br />
-            <div className = "options-container">
-                <button type = "button" id = "Options" onClick={Options}>
-                    {options ? '-' : '+'}
-                </button>
-                {options && ( 
-                    <div>
-                        <label>Date du trajet :</label>
-                        <DatePicker
-                        selected={selectedDate}
-                        onChange={date => handleDateChange(date)}
-                        dateFormat="dd/MM/yyyy"
-                        minDate={new Date(2024, 2, 1)}
-                        maxDate={new Date(2024, 2, 31)}
-                        placeholderText="Sélectionner une date"
-                        />
-                        <br />
-                        <div className="time-picker-container">
+                <div className="options-container">
+                    <button type="button" id="Options" onClick={Options}>
+                        {options ? '-' : '+'}
+                    </button>
+                    {options && (
+                        <div>
+                            <label>Date du trajet :</label>
+                            <DatePicker
+                                selected={selectedDate}
+                                onChange={date => handleDateChange(date)}
+                                dateFormat="dd/MM/yyyy"
+                                minDate={new Date(2024, 2, 1)}
+                                maxDate={new Date(2024, 2, 31)}
+                                placeholderText="Sélectionner une date"
+                            />
+                            <br />
+                            <div className="time-picker-container">
                                 <label>Heure de départ :</label>
                                 <TimePicker
                                     onChange={handleDepartureTimeChange}
@@ -103,33 +132,29 @@ const Formulaire = () => {
                                     clearIcon={null}
                                     disableClock={true}
                                 />
+                            </div>
+                            <br />
+                            <div className="time-picker-container">
+                                <label>Heure d'arrivée :</label>
+                                <TimePicker
+                                    onChange={handleArrivalTimeChange} // -3h si choix d'heure d'arrivée pour appel algo
+                                    value={arrivalTime}
+                                    clearIcon={null}
+                                    disableClock={true}
+                                />
+                            </div>
                         </div>
-                        <br/>
-                        <div className="time-picker-container">
-                            <label>Heure d'arrivée :</label>
-                            <TimePicker
-                                onChange={handleArrivalTimeChange} // -3h si choix d'heure d'arrivée pour appel algo
-                                value={arrivalTime}
-                                clearIcon={null}
-                                disableClock={true}
-                            />
-                        </div>
+                    )}
                 </div>
-                )}
-
-            </div>
-            <br/> 
-
-            <input type="submit" id="submitButton" value="Rechercher" /> {/*onSubmit={handleSubmit} />*/}
-
+                <br />
+                <input type="submit" id="submitButton" value="Rechercher" />
             </form>
         </div>
     );
-}
+};
 
-function Form()
-{
-    return <Formulaire/>;
+function Form() {
+    return <Formulaire />;
 }
 
 export default Form;
